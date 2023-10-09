bc1836d6491bfcb24e0a84fbd8830df0bdff7385
Guilherme <orcinxs@outlook.com>
2023-10-09 17:52:28 -0300

    checks if the zero index of numbers is equal to undefined

--- refactor.js
+++ refactor.js
@@ -8,9 +8,7 @@ let operator;
 let operand1;
 let operand2;
 
-const [ zero ] = numbers;
-
-valuePreview.textContent = zero;
+valuePreview.textContent = numbers[0];
 
 function addDot(arr) {
   switch (arr.length) {
@@ -182,6 +180,10 @@ function removeDot(arr) {
   }
 }
 
+function isNumber(value) {
+  return isNaN(value) ? false : true;
+}
+
 buttons.addEventListener('click', (e) => {
   const { 
     localName, 
@@ -190,14 +192,10 @@ buttons.addEventListener('click', (e) => {
   } = e.target;
 
   const button = localName.includes('button');
-
-  const [ firstNumber ] = numbers;
   
   const hasComma = numbers.join('').includes(',');
 
-  if (firstNumber) {
-    const hasMinusSign = firstNumber.includes('-');
-  }
+  const hasMinusSign = numbers[0] && numbers[0].includes('-');
   
   let lastNumber = numbers[numbers.length -1]; 
 
@@ -210,13 +208,20 @@ buttons.addEventListener('click', (e) => {
     const plus = classList.contains('plus');
     const divide = classList.contains('divide');
     const clear = classList.contains('clear');
+    const equals = classList.contains('equals');
 
     if (number) {
+      if (operand2) {
+        numbers = [ '0' ];
+        operand2 = undefined;
+        operationPreview.textContent = '';
+      }
+
       if (numbers.length < 15) {
         numbers.push(textContent)
       }
 
-      if (firstNumber === zero) {
+      if (numbers[0] === '0') {
         numbers.shift();
       } 
       
@@ -232,16 +237,24 @@ buttons.addEventListener('click', (e) => {
     }
 
     if (plusMinus) {
-      if (firstNumber !== zero && !hasMinusSign) {
-        numbers[0] = '-' + firstNumber;
+      if (numbers[0] === undefined) { // resolver quando equals é clicado e depois number é inserido, sem passar por operators, numbers não é mais resetado, fazendo com que plusMinus funcione.
+        return;
+      }
+
+      if (numbers[0] !== '0' && !hasMinusSign) {
+        numbers[0] = '-' + numbers[0];
       } else {
-        numbers[0] = firstNumber.replace('-', '');
+        numbers[0] = numbers[0].replace('-', '');
       }
     }
 
     if (del) {
+      if (numbers[0] === undefined) {
+        return;
+      }
+
       if (numbers.length === 1 && !hasComma) {
-        numbers[0] = zero;
+        numbers[0] = '0';
       } else {
         if (!lastNumber.includes(',')) {
           numbers.pop();
@@ -287,9 +300,26 @@ buttons.addEventListener('click', (e) => {
       numbers = [];
     }
 
+    if (equals) {
+      if (isNaN(operand2)) {
+        if (isNaN(value)) {
+          operand2 = operand1;
+        } else {
+          operand2 = value;
+        }
+      }
+
+      if (isNumber(operand2)) {
+        if (operator === '+') {
+          operationPreview.textContent = `${operand1} ${operator} ${operand2} =`;
+          valuePreview.textContent = operand1 += operand2;
+        }
+      } 
+    }
+
     if (clear) {
-      numbers = [ zero ];
-      valuePreview.textContent = zero;
+      numbers = ['0'];
+      valuePreview.textContent = numbers[0];
       operand1 = undefined;
       operator = undefined;
       operationPreview.textContent = '';
@@ -298,8 +328,6 @@ buttons.addEventListener('click', (e) => {
 })
 
 
-function isNumber(value) {
-  return isNaN(value) ? false : true;
-}
+
 
 
